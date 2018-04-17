e657ca72e41eba5a9ff5b677797bb48203c9e809
viquy <viquy@10.22.185.229>
2018-04-17 14:07:57 +0700

    -Create new project about codefights.com using Java programming. -Create package The journey begins and class Add.java

---
+++ src/codefight/intro/thejourneybegins/Add.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codefight.intro.thejourneybegins;
+
+/**
+ *
+ * @author viquy
+ */
+/*
+    Write a function that returns the sum of two numbers.
+*/
+public class Add {
+    int add(int param1, int param2){
+        return param1 + param2;
+    }
+}
